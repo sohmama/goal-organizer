8264d51a4892bee2c5693a1115e015d5b4cb49b0
sohmama <ilmss2810@rd6.so-net.ne.jp>
2021-03-13 13:47:06 +0900

    JavaScriptの記述

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,9 @@
 require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
-require("../calendar");
+
+require("../calendar")
+require("../todo")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
